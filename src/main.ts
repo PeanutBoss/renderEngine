51f9bebcba273e243a0540ff96f60a56589a1955
贺创 <2236402130@qq.com>
2024-01-22 17:07:20 +0800

    重构

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import HTMLParser from './htmlParser'
+import CSSParser from './cssParser'
 
 const html = `
 	<div class =  " wrap box" id="container  "   >
@@ -15,4 +16,24 @@ const htmlTree = htmlParser.parse(html)
 console.log('------------------htmlTree------------------')
 console.log(
 	JSON.stringify(htmlTree, null, 2)
+)
+
+const css = `
+	.box,
+	.warp,
+	li,
+	#container {
+		color: #999;
+		font-size: 15px;
+		border: 1px solid #666;
+	}
+	body, .grand-son {
+		background-color: rgba(247, 247, 247, 0.7);
+	}
+`
+const cssParser = new CSSParser()
+const cssRule = cssParser.parse(css)
+console.log('------------------cssRules------------------')
+console.log(
+	JSON.stringify(cssRule, null, 2)
 )
