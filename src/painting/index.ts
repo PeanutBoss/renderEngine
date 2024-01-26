464f91543ada03eb78aba01c6dde34471bdaf0b2
贺创 <2236402130@qq.com>
2024-01-26 10:27:43 +0800

    1.完成渲染功能  2.导出图片

--- src/painting/index.ts
+++ src/painting/index.ts
@@ -1,34 +1,94 @@
 import type LayoutBox from '../layout/LayoutBox'
-import { NodeType } from '../htmlParser/dom'
-import type { Canvas, CanvasRenderingContext2D } from 'canvas'
-import { createCanvas } from 'canvas'
-import fs from 'fs'
+import {NodeType} from '../htmlParser/dom'
 
-// 用 import 会报错
-// const { createCanvas } = require('canvas')
-
-function painting(layoutBox: LayoutBox, outputPath = '') {
+export default function painting(layoutBox: LayoutBox) {
 	const { x, y, width, height } = layoutBox.dimensions.content
-	const canvas = createCanvas(width, height)
-	const ctx = canvas.getContext('2d')
+	const canvas = createCanvas(width + x, height + y)
+	const ctx: CanvasRenderingContext2D = canvas.getContext('2d')
 
 	ctx.fillStyle = '#fff'
 	ctx.fillRect(x, y, width, height)
 
 	renderLayoutBox(layoutBox, ctx)
-	createPNG(canvas, outputPath)
+
+	renderExportButton(canvas)
 }
 
-function renderLayoutBox(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D) {
+function createCanvas(width, height): HTMLCanvasElement {
+	const canvas = document.createElement('canvas')
+	canvas.width = width
+	canvas.height = height
+	canvas.style.border = '1px solid #000'
+	document.body.append(canvas)
+	return canvas
+}
+
+function renderLayoutBox(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D, parent?: LayoutBox) {
+	// 开启新一轮的渲染时重置填充色
+	ctx.fillStyle = 'transparent'
 
 	renderBackground(layoutBox, ctx)
 	renderBorder(layoutBox, ctx)
-	renderText(layoutBox, ctx)
-}
-function renderBackground(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D) {}
-function renderBorder(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D) {}
-function renderText(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D) {}
+	renderText(layoutBox, ctx, parent)
 
-function createPNG(canvas: Canvas, outputPath: string) {
-	canvas.createPNGStream().pipe(fs.createWriteStream(outputPath))
+	for (const child of layoutBox.children) {
+		renderLayoutBox(child, ctx, layoutBox)
+	}
+}
+function renderBackground(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D) {
+	const { x, y, width, height } = layoutBox.dimensions.content
+	ctx.fillStyle = layoutBox.styleNode?.value['background-color']
+	ctx.fillRect(x, y, width, height)
+}
+function renderBorder(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D) {
+	const { x, y, width, height } = layoutBox.dimensions.borderBox()
+	const { left, top, right, bottom } = layoutBox.dimensions.border
+	const borderColor = layoutBox.styleNode?.value['border-color']
+	if (!borderColor) return
+
+	ctx.fillStyle = borderColor
+
+	// 上右下左
+	ctx.fillRect(x, y, width, top)
+	ctx.fillRect(x + width - right, y, right, height)
+	ctx.fillRect(x, y + height - bottom, width, bottom)
+	ctx.fillRect(x, y, left, height)
+}
+function renderText(layoutBox: LayoutBox, ctx: CanvasRenderingContext2D, parent?: LayoutBox) {
+	if (layoutBox.styleNode?.node.nodeType === NodeType.Text) {
+		const { x = 0, y = 0, width } = parent?.dimensions.content || {}
+		const styles = layoutBox.styleNode?.value || {}
+		const fontSize = styles['font-size'] || '14px'
+		const fontFamily = styles['font-family'] || 'serif'
+		const fontWeight = styles['font-weight'] || 'normal'
+		const fontStyle = styles['font-style'] || 'normal'
+
+		ctx.fillStyle = '#333'
+		ctx.font = `${fontStyle} ${fontWeight} ${fontSize} ${fontFamily}`
+		console.log(ctx.fillStyle, 'ctx.fillStyle')
+		ctx.fillText(layoutBox.styleNode.node.nodeValue, x, y + parseInt(fontSize), width)
+	}
+}
+
+function renderExportButton(canvas: HTMLCanvasElement) {
+	const btn = document.createElement('button')
+	btn.style.marginTop = '10px'
+	btn.innerText = '导出图片'
+	btn.onclick = () => {
+		exportPNG(canvas)
+	}
+	document.body.append(btn)
+}
+
+function exportPNG(canvas: HTMLCanvasElement) {
+	const dataUrl = canvas.toDataURL('image/png', { type: 'image/png' })
+	let img: HTMLImageElement | null = new Image()
+	img.src = dataUrl
+	let downloadLink: HTMLAnchorElement | null = document.createElement('a')
+	downloadLink.download = '通过渲染引擎生成的图片.png'
+	downloadLink.href = dataUrl
+	downloadLink.click()
+
+	downloadLink = null
+	img = null
 }
